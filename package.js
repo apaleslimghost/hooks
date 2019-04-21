50e86fef03d8778bf0adc9bc01790bfe2cf587f4
bren <bren@153.io>
2019-04-21 17:11:23 +0100

    oh it wants a description

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	name: 'quarterto:hooks',
 	version: '0.0.1',
 	// Brief, one-line summary of the package.
-	summary: '',
+	summary: 'react hooks for meteor data',
 	// URL to the Git repository containing the source code for this package.
 	git: 'https://github.com/quarterto/hooks',
 	// By default, Meteor will default to using README.md for documentation.
